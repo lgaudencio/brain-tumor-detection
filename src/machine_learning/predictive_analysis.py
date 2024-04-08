6f1af7950e45c6eac9fe65e9f5c9152fa107bb72
Luis Gaudencio <luis_g24@live.com>
2024-04-08 10:16:58 +0000

    Add function to plot the prediction probability results

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -7,3 +7,27 @@ from PIL import Image
 from src.data_management import load_pkl_file
 
 
+def plot_predictions_probabilities(pred_proba, pred_class):
+    """
+    Plot prediction probability results
+    """
+
+    prob_per_class = pd.DataFrame(
+        data=[0, 0],
+        index={'Healthy': 0, 'Tumor': 1}.keys(),
+        columns=['Probability']
+    )
+    prob_per_class.loc[pred_class] = pred_proba
+    for x in prob_per_class.index.to_list():
+        if x not in pred_class:
+            prob_per_class.loc[x] = 1 - pred_proba
+    prob_per_class = prob_per_class.round(3)
+    prob_per_class['Diagnostic'] = prob_per_class.index
+
+    fig = px.bar(
+        prob_per_class,
+        x='Diagnostic',
+        y=prob_per_class['Probability'],
+        range_y=[0, 1],
+        width=600, height=300, template='seaborn')
+    st.plotly_chart(fig)
