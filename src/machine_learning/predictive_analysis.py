233d3727b6d45d0437b8d0b6c88583e9f7675c84
Luis Gaudencio <luis_g24@live.com>
2024-04-08 10:22:08 +0000

    Add function to resize the input image

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -31,3 +31,15 @@ def plot_predictions_probabilities(pred_proba, pred_class):
         range_y=[0, 1],
         width=600, height=300, template='seaborn')
     st.plotly_chart(fig)
+
+
+def resize_input_image(img, version):
+    """
+    Reshape image to average image size
+    """
+    
+    image_shape = load_pkl_file(file_path=f"outputs/{version}/image_shape.pkl")
+    img_resized = img.resize((image_shape[1], image_shape[0]), Image.LANCZOS)
+    my_image = np.expand_dims(img_resized, axis=0)/255
+
+    return my_image
