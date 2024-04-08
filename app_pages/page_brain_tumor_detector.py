7393f9d33d6f1ac882225163d9160f567c5d59b0
Luis Gaudencio <luis_g24@live.com>
2024-04-08 16:16:05 +0000

    Add page for brain tumor detection and import needed packages

---
+++ app_pages/page_brain_tumor_detector.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+import pandas as pd
+
+from src.data_management import download_dataframe_as_csv
+from src.machine_learning.predictive_analysis import (
+                                                    load_model_and_predict,
+                                                    resize_input_image,
+                                                    plot_predictions_probabilities
+                                                    )
+
