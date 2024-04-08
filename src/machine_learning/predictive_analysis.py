5c15b42763f0d92ddfdd7f88f46705f362380e67
Luis Gaudencio <luis_g24@live.com>
2024-04-08 10:35:12 +0000

    Add function to load and predict over live images

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -37,9 +37,30 @@ def resize_input_image(img, version):
     """
     Reshape image to average image size
     """
-    
+
     image_shape = load_pkl_file(file_path=f"outputs/{version}/image_shape.pkl")
     img_resized = img.resize((image_shape[1], image_shape[0]), Image.LANCZOS)
     my_image = np.expand_dims(img_resized, axis=0)/255
 
     return my_image
+
+
+def load_model_and_predict(my_image, version):
+    """
+    Load and perform ML prediction over live images
+    """
+
+    model = load_model(f"outputs/{version}/brain-tumor-detection.h5")
+
+    pred_proba = model.predict(my_image)[0, 0]
+
+    target_map = {v: k for k, v in {'Healthy': 0, 'Tumor': 1}.items()}
+    pred_class = target_map[pred_proba > 0.5]
+    if pred_class == target_map[0]:
+        pred_proba = 1 - pred_proba
+
+    st.write(
+        f"The predictive analysis indicates that the sample brain MRI scan shows "
+        f"**{pred_class.lower()}**.")
+
+    return pred_proba, pred_class
