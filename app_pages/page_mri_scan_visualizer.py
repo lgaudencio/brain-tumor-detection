7577d171b22e08199a71cdc5d3f1bfd63c099e23
Luis Gaudencio <luis_g24@live.com>
2024-04-08 13:54:21 +0000

    Add page for mri scan visualizer and import needed packages

---
+++ app_pages/page_mri_scan_visualizer.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+import itertools
+import random
+
