51e91e0e82fdc3e9e383bbb9ce5400d1236ceb94
Luis Gaudencio <luis_g24@live.com>
2024-04-08 10:06:38 +0000

    Add predictive analysis py file and import needed packages

---
+++ src/machine_learning/predictive_analysis.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+from tensorflow.keras.models import load_model
+from PIL import Image
+from src.data_management import load_pkl_file
+
+
