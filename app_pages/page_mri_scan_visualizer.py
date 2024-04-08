bea842194c6340e248eba0092e16dd4caa704168
Luis Gaudencio <luis_g24@live.com>
2024-04-08 14:54:41 +0000

    Add function for image montage

--- app_pages/page_mri_scan_visualizer.py
+++ app_pages/page_mri_scan_visualizer.py
@@ -53,3 +53,50 @@ def page_mri_scan_visualizer_body():
                       label_to_display=label_to_display,
                       nrows=8, ncols=3, figsize=(10,25))
       st.write("---")
+
+
+def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15,10)):
+  sns.set_style("white")
+  labels = os.listdir(dir_path)
+
+  # Subset the class you are interested to display
+  if label_to_display in labels:
+
+    # Checks if your montage space is greater than subset size
+    # how many images in that folder
+    images_list = os.listdir(dir_path+'/'+ label_to_display)
+    if nrows * ncols < len(images_list):
+      img_idx = random.sample(images_list, nrows * ncols)
+    else:
+      print(
+          f"Decrease nrows or ncols to create your montage. \n"
+          f"There are {len(images_list)} in your subset. "
+          f"You requested a montage with {nrows * ncols} spaces")
+      return
+    
+
+    # Create list of axes indices based on nrows and ncols
+    list_rows= range(0,nrows)
+    list_cols= range(0,ncols)
+    plot_idx = list(itertools.product(list_rows,list_cols))
+
+
+    # Create a Figure and display images
+    fig, axes = plt.subplots(nrows=nrows,ncols=ncols, figsize=figsize)
+    for x in range(0,nrows*ncols):
+      img = imread(dir_path + '/' + label_to_display + '/' + img_idx[x])
+      img_shape = img.shape
+      axes[plot_idx[x][0], plot_idx[x][1]].imshow(img)
+      axes[plot_idx[x][0], plot_idx[x][1]].set_title(
+        f"Width {img_shape[1]}px x Height {img_shape[0]}px"
+      )
+      axes[plot_idx[x][0], plot_idx[x][1]].set_xticks([])
+      axes[plot_idx[x][0], plot_idx[x][1]].set_yticks([])
+    plt.tight_layout()
+    
+    st.pyplot(fig=fig)
+
+
+  else:
+    print("The label you selected doesn't exist.")
+    print(f"The existing options are: {labels}")
