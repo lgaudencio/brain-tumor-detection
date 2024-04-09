f2711336f6f2f28623aa7de0cdf4c7d910e1f6d7
Luis Gaudencio <luis_g24@live.com>
2024-04-09 18:17:33 +0000

    Add page scripts and app pages

--- app.py
+++ app.py
@@ -1,2 +1,22 @@
 import streamlit as st
 from app_pages.multipage import MultiPage
+
+
+# load pages scripts
+from app_pages.page_summary import page_summary_body
+from app_pages.page_mri_scan_visualizer import page_mri_scan_visualizer_body
+from app_pages.page_brain_tumor_detector import page_brain_tumor_detector_body
+from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_ml_performance import page_ml_performance_metrics
+
+# Create an instance of the app
+app = MultiPage(app_name="Brain Tumor Detection") 
+
+# Add your app pages here using .add_page()
+app.add_page("Quick Project Summary", page_summary_body)
+app.add_page("MRI Scan Visualiser", page_mri_scan_visualizer_body)
+app.add_page("Brain Tumor Detection", page_brain_tumor_detector_body)
+app.add_page("Project Hypothesis", page_project_hypothesis_body)
+app.add_page("ML Performance Metrics", page_ml_performance_metrics)
+
+app.run()  # Run the app
