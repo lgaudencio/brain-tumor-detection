aa944286610527aba98da697963e30b9509d3f26
Luis Gaudencio <luis_g24@live.com>
2024-04-09 10:58:54 +0000

    Add performace images to hypothesis 2

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -61,5 +61,9 @@ def page_project_hypothesis_body():
         f"performed better than the Sigmoid function."
     )
 
-    
+    softmax_model = plt.imread(f"streamlit_images/softmax_model.png")
+    st.image(softmax_model, caption='SoftMax Loss/Accuracy Performance')
+
+    sigmoid_model = plt.imread(f"streamlit_images/sigmoid_model.png")
+    st.image(sigmoid_model, caption='Sigmoid Loss/Accuracy Performance')
 
